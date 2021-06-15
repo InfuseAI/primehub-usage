175712cd98cc16d94f0979740a4dd8e6b398c902
Kent Huang <kentwelcome@gmail.com>
2021-06-15 17:09:40 +0800

    [Feature] add github username and token into release-image.sh
    Signed-off-by: Kent Huang <kentwelcome@gmail.com>

--- release-image.sh
+++ release-image.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 VERSION=$(cat VERSION)
 DRY_RUN=false
+GITHUB_USERNAME=${GITHUB_USERNAME:-}
+GITHUB_TOKEN=${GITHUB_TOKEN:-}
 
 info() {
   echo -e "\033[0;32m$1\033[0m"
@@ -30,8 +32,23 @@ info "[Release] Primehub-usage with version $VERSION"
 echo "  name: $name"
 echo "  tag:  $tag"
 
+if [ "$GITHUB_USERNAME" == "" ]; then
+  read -p "Please provide Github username: " GITHUB_USERNAME
+fi
+if [ "$GITHUB_TOKEN" == "" ]; then
+  read -s -p "Please provide Github token: " GITHUB_TOKEN
+  echo ""
+fi
+
+info "[Auth] Github"
+echo "Username: $GITHUB_USERNAME"
+echo "Token:    $(echo "$GITHUB_TOKEN" | sed "s/\(.\{6\}\).*/\1********/g")"
+
 if [ "$DRY_RUN" == false ]; then
+  info "[Create] release $name with tag $tag"
   curl \
+    -i \
+    -u ${GITHUB_USERNAME}:${GITHUB_TOKEN} \
     -X POST \
     -H "Accept: application/vnd.github.v3+json" \
     https://api.github.com/repos/infuseai/primehub-usage/releases \
